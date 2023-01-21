9e5a452b3c83b3c1696fc2c289d8303441d92360
ShootingRing <1433057606@qq.com>
2023-01-21 09:12:39 +0800

    SQL

---
+++ VuePro1/SQL/user.sql
@@ -0,0 +1,15 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+INSERT INTO `user` VALUES ('admin', '123456', 1);
+INSERT INTO `user` VALUES ('Wiosier', 'WOWring000', 2);
+
+SET FOREIGN_KEY_CHECKS = 1;
